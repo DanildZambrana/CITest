b95669b5c65d0fc50e86d1ba2d47109e43962d93
DanildZambrana <danildjohan12@gmail.com>
2022-01-10 17:50:34 -0600

    added test

---
+++ src/test/java/com/github/danildzambrana/citest/CITestTest.java
@@ -0,0 +1,14 @@
+package com.github.danildzambrana.citest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CITestTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
