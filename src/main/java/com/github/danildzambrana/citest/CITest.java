5787543da908f3e9d058bca26a68ffb2ef9c1a1e
DanildZambrana <danildjohan12@gmail.com>
2022-01-10 15:21:40 -0600

    added change to test CI in jenkins

--- src/main/java/com/github/danildzambrana/citest/CITest.java
+++ src/main/java/com/github/danildzambrana/citest/CITest.java
@@ -3,5 +3,7 @@ package com.github.danildzambrana.citest;
 public class CITest {
     public static void main(String[] args) {
         System.out.println("Correct!");
+
+        System.out.println("Correct 2!");
     }
 }
